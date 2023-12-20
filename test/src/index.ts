da7b5fa4e0bc03767fe457433da95c1f8e6f14dd
paolodelfino <hackymail12@gmail.com>
2023-12-20 14:53:59 -0800

    Add putesting

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,7 +1,47 @@
 import { assert } from "chai";
 import {} from "../../dist/index.mjs";
 import { stopwatch } from "./utils";
+import { parseArgs } from "util";
+import { Test_Set } from "putesting";
+import "./utils";
 
-async function main() {}
+type Tests = "";
+
+async function main() {
+  const tests = new Test_Set<Tests>({
+    "": {
+      async callback() {},
+    },
+  });
+
+  const argc = process.argv.length - 2;
+  const argv = process.argv.slice(2);
+
+  if (argc == 0) {
+    await tests.run();
+  } else {
+    const { values, positionals } = parseArgs({
+      args: argv,
+      allowPositionals: true,
+      options: {
+        list: {
+          type: "boolean",
+        },
+      },
+    });
+
+    if (values.list) {
+      console.log(tests.keys);
+      return;
+    }
+
+    for (const arg of positionals) {
+      if (!tests.has(arg)) {
+        throw new Error(`"${arg}" is not a valid test`);
+      }
+      await tests.run(arg);
+    }
+  }
+}
 
 main();
